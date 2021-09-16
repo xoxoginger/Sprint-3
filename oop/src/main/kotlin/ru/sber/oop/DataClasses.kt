f920ce17a55ebaf327fb5f1f0eaa67eeef3e0568
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-16 15:02:09 +0300

    it works (:

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,26 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+    //par.2
+    override fun equals(other: Any?): Boolean {
+        if (other is User) 
+            return (this.name == other.name &&
+                    this.age == other.age &&
+                    this.city == other.city)
+                    
+        else return false
+        }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    //par.1
+    val user2 = user1.copy(name = "Juli")
+    //par.2
+    user1.city = "Omsk"
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+    
+    println(user3.city)
+    println(user1.equals(user3))
 }
