08f9202b5bf168a825a0675b4ed65e65d46699e6
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-20 00:48:45 +0300

    finally works

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,12 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +18,26 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val inputFile = "logfile.log"
+        val outputFile = "logfile.zip"
+        
+        val fis = FileInputStream(inputFile)
+        val zos = ZipOutputStream(FileOutputStream(outputFile))
+            
+        var buffer = ByteArray(1024)
+        
 
+        fis.use {
+            buffer = fis.readBytes()
+            fis.close()
+        }
+
+        zos.use {
+            zos.putNextEntry(ZipEntry(inputFile))
+            zos.write(buffer)
+            zos.closeEntry()
+            zos.close()
+        }
     }
 
     /**
@@ -19,6 +45,30 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        val inputFile = File("logfile.zip")
+        val outputFile = File("unzippedLogfile.log")
 
-    }
+        val zis = ZipInputStream(FileInputStream(inputFile))
+        val fos = FileOutputStream(outputFile)
+        var buffer = ByteArray(1024)
+
+        zis.use{
+            zis.nextEntry
+            buffer = zis.readBytes()
+            zis.closeEntry()
+            zis.close()
+        }
+
+        fos.use {
+            fos.write(buffer)
+            fos.close()
+        }
+   }
+}
+
+
+fun main() {
+    val archivator = Archivator()
+    archivator.zipLogfile()
+    archivator.unzipLogfile()
 }
