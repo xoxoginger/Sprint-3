1706a929442c7074843bc2a59eb2b84eb56fed67
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-20 04:09:23 +0300

    4:09 am..........

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,7 +1,13 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.util.stream.Collectors
+import java.nio.charset.Charset
+import kotlin.io.path.*
 /**
- * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
+ * Реализовать простой аналог утилиты grep с использованием классов из пакета java.nio.
  */
 class Grep {
     /**
@@ -15,6 +21,22 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val filePaths = Paths.get("io/logs")
+        val resultFile = File("io/result.txt")
+        val files = Files.walk(filePaths).filter(Files::isRegularFile).collect(Collectors.toList())
+        val buffWriter = Files.newBufferedWriter(resultFile.toPath(), Charset.forName("UTF-8"))
 
+        buffWriter.use {
+            files.forEach { file ->
+                var lineIndex = 0
+                file.forEachLine { line ->
+                    lineIndex++
+                    if (line.contains(subString)) {
+                        buffWriter.write("${file.fileName} : $lineIndex : $line" + System.lineSeparator())
+                        buffWriter.flush()
+                    }
+                }
+            }
+        }
     }
 }
